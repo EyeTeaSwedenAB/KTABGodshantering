a115d16413d4fa79b8acf78f11d47850b9a9b1d0
PeterBOberg <peter.boberg@gmail.com>
2016-08-22 23:10:35 +0200

    New cleanup

--- src/main/java/com/peter/startup/MainApp.java
+++ src/main/java/com/peter/startup/MainApp.java
@@ -1,6 +1,9 @@
 package com.peter.startup;
 
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
 
 public class MainApp extends Application {
@@ -11,6 +14,14 @@ public class MainApp extends Application {
     }
 
     public void start(Stage stage) throws Exception {
+
+        FXMLLoader loader = new FXMLLoader();
+        BorderPane mainWindow = loader.load(getClass().getResource("/fxml/MainWindow.fxml"));
+        BorderPane inputWindow = loader.load(getClass().getResource("/fxml/InputWindow.fxml"));
+        mainWindow.setCenter(inputWindow);
+
+        stage.setScene(new Scene(mainWindow));
+        stage.show();
 
 
     }
